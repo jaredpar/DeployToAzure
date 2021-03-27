cb5313dd131711b50a418357ddda0664837f66e0
Jared Parsons <jaredpparsons@gmail.com>
2021-03-27 13:45:08 -0700

    Add the OS description

--- Util/PageInfo.cs
+++ Util/PageInfo.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Util
 {
     public class PageInfo
     {
-        public string Greeting { get; set; } = "Util Hello World Again";
+        public string Greeting { get; set; } = "Util Hello World Again On " + RuntimeInformation.OSDescription;
     }
 }
